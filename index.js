a77550b6c121504ad30b83046c677e077fb86c3d
pbnj1 <105159702+pbnj1@users.noreply.github.com>
2022-07-01 17:08:12 -0400

    formatted styling and cleaned up the code more

--- index.js
+++ index.js
@@ -7,59 +7,36 @@ TitleText.append(timeStamp);
 // end of top section with the title and time sta
 
 // Save button function that will save to local storage
-$(".saveBtn").on("click", function(){
-
-    
-    // the code below works when called by the onclick above - but only for one
-    var textInput1 = $("#text1").val();
-    localStorage.setItem("textInput1", textInput1);
-    var textInput2 = $("#text2").val();
-    localStorage.setItem("textInput2", textInput2);
-    var textInput3 = $("#text3").val();
-    localStorage.setItem("textInput3", textInput3);
-    var textInput4 = $("#text4").val();
-    localStorage.setItem("textInput4", textInput4);
-    var textInput5 = $("#text5").val();
-    localStorage.setItem("textInput5", textInput5);
-    var textInput6 = $("#text6").val();
-    localStorage.setItem("textInput6", textInput6);
-    var textInput7 = $("#text7").val();
-    localStorage.setItem("textInput7", textInput7);
-    var textInput8 = $("#text8").val();
-    localStorage.setItem("textInput8", textInput8);
-    var textInput9 = $("#text9").val();
-    localStorage.setItem("textInput9", textInput9);
-    var textInput10 = $("#text10").val();
-    localStorage.setItem("textInput10", textInput10);
-    var textInput11 = $("#text11").val();
-    localStorage.setItem("textInput11", textInput11);
-    var textInput12 = $("#text12").val();
-    localStorage.setItem("textInput12", textInput12);
-
-
-
-
-
-    // function saveText() {
-    //     // tried to save multiple values to an object...
-    //     var todo = {
-    //       eight: $(".text-area").val(),
-    //       nine: $(".text-area").val(),
-    //       ten: $(".text-area").val(),
-    //     };
-    //     // Use .setItem() to store object in storage and JSON.stringify to convert it as a string
-    //     localStorage.setItem("saveText", JSON.stringify(saveText));
-     
-    // var textArr = querySelectorAll(".text-area")
-    // for (let i = 0; i < textArr.length; i++ ){
-    //     if("<textarea>" = "")
-    // }
-}
-
-);
+$(".saveBtn").on("click", function () {
+  // the code below works when called by the onclick above - but only for one
+  var textInput1 = $("#text1").val();
+  localStorage.setItem("textInput1", textInput1);
+  var textInput2 = $("#text2").val();
+  localStorage.setItem("textInput2", textInput2);
+  var textInput3 = $("#text3").val();
+  localStorage.setItem("textInput3", textInput3);
+  var textInput4 = $("#text4").val();
+  localStorage.setItem("textInput4", textInput4);
+  var textInput5 = $("#text5").val();
+  localStorage.setItem("textInput5", textInput5);
+  var textInput6 = $("#text6").val();
+  localStorage.setItem("textInput6", textInput6);
+  var textInput7 = $("#text7").val();
+  localStorage.setItem("textInput7", textInput7);
+  var textInput8 = $("#text8").val();
+  localStorage.setItem("textInput8", textInput8);
+  var textInput9 = $("#text9").val();
+  localStorage.setItem("textInput9", textInput9);
+  var textInput10 = $("#text10").val();
+  localStorage.setItem("textInput10", textInput10);
+  var textInput11 = $("#text11").val();
+  localStorage.setItem("textInput11", textInput11);
+  var textInput12 = $("#text12").val();
+  localStorage.setItem("textInput12", textInput12);
+});
 
 //retrieving the saved data and getting it on the screen
-  onload = function () {
+onload = function () {
   var textRetrieve1 = localStorage.getItem("textInput1");
   document.getElementById("text1").value = textRetrieve1;
   var textRetrieve2 = localStorage.getItem("textInput2");
@@ -91,115 +68,33 @@ var localTime = moment().format("H");
 // console.log(localTime);
 //////////////////////////////////
 
-
-{/* <div id="demo">Example div</> */}
-// let newValue = document.getElementById('text1').getAttribute('data-value');
-// console.log(newValue)
-
-
-// for (let index = 0; index < 10; index++) {
-//   let newValue = document.getElementById('text').getAttribute('data-value');
-//   console.log(newValue)
-// }
-
-
-
-
 // Functionallity of the textbox changing color to the time
 
-var dataArr = document.querySelectorAll(".text-area")
+var dataArr = document.querySelectorAll(".text-area");
 // console.log(dataArr)
 
-for(let i = 0; i < dataArr.length; i++){
+for (let i = 0; i < dataArr.length; i++) {
+  // var newDA = dataArr[i].attr("data-value")
+  var value = dataArr[i].dataset.value;
+  var currentTime = moment().format("H");
 
+  console.log(dataArr[i].dataset.value);
+  // console.log($(".text-area"))
 
+  // console.log(currentTime)
+  // console.log(value)
 
-// var newDA = dataArr[i].attr("data-value")
-var value = dataArr[i].dataset.value
-var currentTime = moment().format("H"); 
-
-console.log(dataArr[i].dataset.value)
-// console.log($(".text-area"))
-
-console.log(currentTime)
-console.log(value)
-
-
-
-
-if(value < currentTime){
-  console.log("this is past")
-  dataArr[i].classList.add("past")
-}
-else if( value === currentTime){
-    console.log("this is present")
+  if (value < currentTime) {
+    console.log("this is past");
+    dataArr[i].classList.add("past");
+  } else if (value === currentTime) {
+    console.log("this is present");
     // dataArr[i].classList.remove("past")
-    dataArr[i].classList.add("present")
+    dataArr[i].classList.add("present");
+  } else {
+    console.log("this is future");
+    // dataArr[i].classList.remove("past")
+    // dataArr[i].classList.remove("present")
+    dataArr[i].classList.add("future");
   }
-  else{
-      console.log("this is future")
-      // dataArr[i].classList.remove("past")
-      // dataArr[i].classList.remove("present")
-      dataArr[i].classList.add("future")}
-
-
-
-
-
-// if(value < currentTime){
-//     console.log("this is past")
-//     dataArr[i].classList.add("past")
-//   }
-//   else if( value === currentTime){
-//       console.log("this is present")
-//       dataArr[i].classList.remove("past")
-//       dataArr[i].classList.add("present")
-//     }
-//     else{
-//         console.log("this is future")
-//         dataArr[i].classList.remove("past")
-//         dataArr[i].classList.remove("present")
-//         dataArr[i].classList.add("future")}
-
-
-
-//  if(value < currentTime){
-//   console.log("this is past")
-//   dataArr[i].classList.add("past")
-// }if( value === currentTime){
-//   console.log("this is present")
-//   dataArr[i].classList.remove("past")
-//   dataArr[i].classList.add("present")
-// }else{
-//   console.log("this is future")
-//   dataArr[i].classList.remove("past")
-//   dataArr[i].classList.remove("present")
-//   dataArr[i].classList.add("future")
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-//below has appropriate sized text box
-// if(value < currentTime){
-//     $("#text1").addClass("past")
-// }else if( value == currentTime){
-// $("#text1").addClass("present")
-// }else($("#text1").addClass("future"))
-
-
-
-
-// setInterval(() => {
-//     var time = moment().format("h:mm:ss a");
-//   }, 1000);
-//   console.log(time);
-  
+}
